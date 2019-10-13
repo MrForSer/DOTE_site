38a02db0ee745391fc9ed4e2229cecfcb9eaa6ca
Forser <mr.forser@gmail.com>
2019-10-13 18:32:50 +0700

    new web site

---
+++ script.js
@@ -0,0 +1,16 @@
+function slowScroll(id) {
+    $('html, body').animate({
+        scrollTop: $(id).offset().top
+    }, 500);
+}
+
+$(document).on("scroll", function () {
+    if ($(window).scrollTop() === 0)
+        $("header").removeClass("fixed");
+    else $("header").attr("class", "fixed");
+})
+
+function login() {
+    document.getElementById("hide").style.display = "block";
+    document.getElementById("window").style.display = "block";
+}
