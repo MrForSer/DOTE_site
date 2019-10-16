a8d9c168c63f8d6a2eb6a840a1fa8eb4abd7c799
Forser <mr.forser@gmail.com>
2019-10-17 01:46:03 +0700

    cosmetic

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ function closeForm() {
 function validate() {
     var login = document.getElementById('login').value;
     var password = document.getElementById('password').value;
-    if((login == "admin") && (password == "admin")){
+    if ((login == "admin") && (password == "admin")) {
         alert("Данные верны");
-    } else if (login == ""){
-            alert("Введите логин");
-    } else if (password == ""){
-        alert("Введите пароль")}
-    else
+    } else if (login == "") {
+        alert("Введите логин");
+    } else if (password == "") {
+        alert("Введите пароль")
+    } else
         alert("Логин или пароль указаны некорректно");
 }
 
@@ -37,19 +37,19 @@ function sendMessage() {
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
     var message = document.getElementById('message').value;
-    if(name == ""){
+    if (name == "") {
         alert("Укажите ваше имя");
-    } else if (email == ""){
+    } else if (email == "") {
         alert("Введите адрес электронной почты");
-    } else if (validateEmail(email) == false){
-        alert ("Убедитесь, что адрес электронной почты введен корректно")
-    } else if (message == ""){
-        alert("Введите ваше сообщение")}
-    else
+    } else if (validateEmail(email) == false) {
+        alert("Убедитесь, что адрес электронной почты введен корректно")
+    } else if (message == "") {
+        alert("Введите ваше сообщение")
+    } else
         alert("Сообщение сохранено");
 }
 
 function validateEmail(email) {
-  var re = /\S+@\S+\.\S+/;
-  return re.test(email);
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
