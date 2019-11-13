b08dc75d506bf201336ad7a4cc62fcf148bca70f
forser <mr.forser@gmail.com>
2019-11-13 17:20:28 +0700

    customer changes

---
+++ src/main/java/app/validators/CustomerValidator.java
@@ -0,0 +1,10 @@
+package app.validators;
+
+import app.entities.Customer;
+
+public class CustomerValidator extends BeanValidator<Customer> {
+
+    public CustomerValidator() {
+        super();
+    }
+}
