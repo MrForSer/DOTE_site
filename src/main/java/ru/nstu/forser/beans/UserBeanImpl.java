6af907770c8e3feb1316f160c24d9a65c04428a9
Forser <mr.forser@gmail.com>
2019-12-11 16:04:03 +0700

    Fix UserBeanImpl - add driver

--- src/main/java/ru/nstu/forser/beans/UserBeanImpl.java
+++ src/main/java/ru/nstu/forser/beans/UserBeanImpl.java
@@ -11,6 +11,8 @@ public class UserBeanImpl implements UserBean {
     private static final String DB_URL = "jdbc:postgresql://localhost:5432/dote";
     private static final String DB_USERNAME = "postgres";
     private static final String DB_PASSWORD = "postgres";
+    private static final String DRIVER = "org.postgresql.Driver";
+
 
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
@@ -19,21 +21,24 @@ public class UserBeanImpl implements UserBean {
     @Override
     public List<User> getUsers() {
         List<User> returnValue = new ArrayList<>();
-        try (Connection connection = getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                try (ResultSet resultSet = statement.executeQuery("SELECT id, login, password, firstname, lastname FROM users")) {
-                    while (resultSet.next()) {
-                        returnValue.add(new User(
-                                resultSet.getInt(1),
-                                resultSet.getString(2),
-                                resultSet.getString(3),
-                                resultSet.getString(4),
-                                resultSet.getString(5)
-                        ));
+        try {
+            Class.forName(DRIVER);
+            try (Connection connection = getConnection()) {
+                try (Statement statement = connection.createStatement()) {
+                    try (ResultSet resultSet = statement.executeQuery("SELECT id, login, password, firstname, lastname FROM users")) {
+                        while (resultSet.next()) {
+                            returnValue.add(new User(
+                                    resultSet.getInt(1),
+                                    resultSet.getString(2),
+                                    resultSet.getString(3),
+                                    resultSet.getString(4),
+                                    resultSet.getString(5)
+                            ));
+                        }
                     }
                 }
             }
-        } catch (SQLException ex) {
+        } catch (SQLException | ClassNotFoundException ex) {
             ex.getMessage();
         }
         return returnValue;
